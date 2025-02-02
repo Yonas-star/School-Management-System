c0426e5e5e3085461dba97fc0de764623777b62f
Yonas Kassahun <yonaskassahunyoka@gmail.com>
2025-02-02 13:22:34 +0300

    hohouh

--- School/src/Components/LoginPage.jsx
+++ School/src/Components/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../App.css';
+import logo from "../../public/JVSA.jpg"
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
